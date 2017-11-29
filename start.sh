c483a65c5fda6133a33957501d54e2a337ae9b19
slythern <31569641+slythern@users.noreply.github.com>
2017-11-29 14:11:23 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 sleep 1
-#node resin-wifi-connect/src/app.js --clear=false
 ./wifi-connect --clear=false
 
 # At this point the WiFi connection has been configured and the device has
