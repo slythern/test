7650194f050ff43c57e08879a6532f77332e5639
slythern <31569641+slythern@users.noreply.github.com>
2017-11-29 13:26:13 -0600

    Update start.sh
    updeded resin.wifi.connect

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 sleep 1
-node resin-wifi-connect/src/app.js --clear=false
+#node resin-wifi-connect/src/app.js --clear=false
+./wifi-connect --clear=false
 
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
@@ -19,3 +20,7 @@ umount /dev/shm && mount -t tmpfs shm /dev/shm
 
 rm /tmp/.X0-lock &>/dev/null || true
 startx /usr/src/app/connectedframe.py
+
+while true; do
+	sleep 100
+done
